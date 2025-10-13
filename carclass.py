8b1cdefe6aa06ade9b21a5412313e2500d9e057f
bhavanamajji7 <bhavanamajji7@gmail.com>
2025-10-13 10:10:23 +0530

    Add files via upload

---
+++ carclass.py
@@ -0,0 +1,8 @@
+class Car:
+def  init (self, make, model, year): self.make = make
+self.model = model self.year = year
+def start(self):
+print(f"The {self.year} {self.make} {self.model} is starting.") def stop(self):
+print(f"The {self.year} {self.make} {self.model} is stopping.") # Example usage
+car1 = Car("Toyota", "Camry", 2022) car2=Car("Toyota", "Camry", 2032) car1.start()
+car2.stop()
---
+++ split.py
@@ -0,0 +1,4 @@
+input_string = "Python Programming is very very easy!"
+char_array = list(input_string)
+print("Array of characters:", char_array)
+
